40a2bac80d06a9397fbd4233aab3cc580b92d4e4
joaogilo-hub <joao.gilo@edu.senai.br>
2026-03-17 14:07:01 -0300

    aprendendo sobre dino.shift

--- 08-vetor.js
+++ 08-vetor.js
@@ -21,3 +21,31 @@ dinos.unshift("Velocirapitor");
 console.log("1ª posição:", dinos[0])
 console.log("4ª posição:", dinos[3])
 console.log("20ª posição (não exite):", dinos[20])
+
+// pop - > Remover um elemento do final do vetor (ultimo elemento)
+dinos.pop();
+console.table(dinos);
+console.log("O vetor agora tem", dinos.length, "elementos");
+
+// shift- > Remover um elemento do final do vetor (primeiro elemento)
+dinos.shift();
+console.table(dinos);
+console.log("O vetor agora tem", dinos.length, "elementos");
+
+// spice - > Removendo um elemento a partir de um indice, e tembem precisa infromada a quantidade de registro a ser excluido a partir do indice
+dinos.splice(1, 1)// Remove somente o segundo item da lista
+console.log("O vetor agora tem", dinos.length, "elementos")
+
+// Adicionar mais elementos ao vetor
+dinos.push("Pterodáctilo")
+dinos.push("Estragossauro")
+dinos.push("Argentinossauro")
+
+// Procurar mu elemento a partir do nome (descrição)
+// indexOf(procurado) - > retorna o indice do elemento no vetor
+let procurando = "Estragossauro"
+let indece = dinos.indexOf(procurado)
+console.log("O ", procurando, "está no indice:", indice)
+
+// Excluir um elemente 
+
