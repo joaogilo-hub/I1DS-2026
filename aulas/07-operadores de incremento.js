4149804507f5e86956eb9413d5d1ec56609ef12a
joaogilo.hub <joao.gilo@edu.senai.br>
2026-03-24 11:05:48 -0300

    .

---
+++ aulas/07-operadores de incremento.js
@@ -0,0 +1,24 @@
+// Operadores de incremento e decremento | pré e pós incremento
+// incremento => somar algo a alguma variável
+// decremento => sibtrair algo de alguma coisa terrivel
+
+let idade = 16
+console.log(idade);
+
+//idade = +1;// somei 1 na idade
+idade++;
+console.log(idade);
+idade++;
+console.log(idade);
+
+//idade = +1;// somei 1 na idade
+idade--;
+console.log(idade);
+console.log("-------------------------")
+
+let novaIdade = idade++
+console.log("idade:", idade, "| Nova idade:" , novaIdade);
+
+novaIdade = ++idade
+console.log("idade:", idade , "| Nova idade:" , novaIdade);
+
---
+++ aulas/12-lacoRepeticao.js
@@ -0,0 +1,4 @@
+// laco de Repetiçao
+// o professor te cocolocou de castigo e pediu para vc escrever mil vesez a frease
+// "Eu prestar mais atenção nas aulas!"
+
