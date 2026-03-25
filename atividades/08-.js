0282fa01853fb95ec7d219aad71a51afd7a6d1fd
joaogilo.hub <joao.gilo@edu.senai.br>
2026-03-25 10:35:53 -0300

    .

--- atividades/08-.js
+++ atividades/08-.js
@@ -37,3 +37,55 @@ for (let i = 0; i < matriz.length; i++) {
     matriz[i] *= 3; // numeros [i] = numeros [i] * 3;
 }
 console.table(matriz)
+
+var nome = "joao vitor"
+var idade = 17
+var cidade = "jau"
+
+console.log(
+  "Meu nome é",
+  nome,
+  "eu tenho",
+  idade,
+  "anos",
+  "e moro em",
+  cidade,
+  ".",
+);
+
+let x = 20;
+let y = 4;
+
+console.log("X = ", x, "| Y = ", y);
+
+resultado = x + y;
+console.log("A soma é", resultado);
+
+resultado = x - y;
+console.log("A subtração é", resultado);
+
+resultado = x * y;
+console.log("A multiplicação é", resultado);
+
+resultado = x / y;
+console.log("A divisão é", resultado);
+
+let peso = 70;
+let altura = 1.7;
+
+let IMC = peso / (altura * altura);
+
+console.log("O seu peso é:", peso);
+console.log("A sua altura é:", altura);
+console.log("Então o seu IMC é:", IMC);
+
+let n1 = 8;
+let n2 = 10;
+let n3 = 6;
+
+let media = (n1 + n2 + n3) / 4;
+
+console.log("Nota da primeira prova:", n1);
+console.log("Nota da segunda prova:", n2);
+console.log("Nota da terceira prova:", n3);
+console.log("A sua média é:", media);
